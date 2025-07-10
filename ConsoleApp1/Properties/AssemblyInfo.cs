805cdeee5dad321ea48b24e7ecef5bb7e3966b37
LeonardoPenas <n00460162@upn.pe>
2025-07-09 22:24:01 -0500

    Add files via upload

--- ConsoleApp1/Properties/AssemblyInfo.cs
+++ ConsoleApp1/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
-[assembly: AssemblyTitle("MiBiblioteca")]
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiBiblioteca")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("4f36b3e7-95b0-468f-89b4-965d250b8afa")]
+[assembly: Guid("d36773b9-d767-4064-bb9e-93a66c5180e2")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
---
+++ MiBiblioteca/Clase_EP1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MiBiblioteca
+{
+    public class Clase_EP1
+    {
+        int[] dias = new int[7] { 1, 2, 3, 4, 5, 6, 7 }; //representa los dias de la semana
+        int[,] numeros = new int[7, 7];
+        int[] resultado = new int[7];
+        int[] articulos = new int[7];
+
+
+
+        public void articulo1()
+        {
+
+            int col = 0;
+            Random r = new Random();
+            Console.WriteLine("Datos de Lunes");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la primera columna
+                Thread.Sleep(300);
+            }
+
+
+        }
+        public void articulo2()
+        {
+            int col = 1;
+            Random r = new Random();
+            Console.WriteLine("Datos de Martes");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la segunda columna
+                Thread.Sleep(300);
+            }
+        }
+        public void articulo3()
+        {
+            int col = 2;
+            Random r = new Random();
+            Console.WriteLine("Datos de Miercoles");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la tercera columna
+                Thread.Sleep(300);
+            }
+        }
+        public void articulo4()
+        {
+            int col = 3;
+            Random r = new Random();
+            Console.WriteLine("Datos de Jueves");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la cuarta columna
+                Thread.Sleep(300);
+            }
+        }
+        public void articulo5()
+        {
+            int col = 4;
+            Random r = new Random();
+            Console.WriteLine("Datos de Viernes");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la quinta columna
+                Thread.Sleep(300);
+            }
+        }
+        public void articulo6()
+        {
+            int col = 5;
+            Random r = new Random();
+            Console.WriteLine("Datos de Sábado");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la sexta columna
+                Thread.Sleep(300);
+            }
+        }
+        public void articulo7()
+        {
+            int col = 6;
+            Random r = new Random();
+            Console.WriteLine("Datos de Domingo");
+            for (int i = 0; i < numeros.GetLength(0); i++)//muestra la cantidad de valores por dimension
+            {
+                int unidades = r.Next(0, 201);
+                Console.WriteLine(dias[i] + ": " + unidades);
+                numeros[i, col] = unidades; //accede a la septima columna
+                Thread.Sleep(300);
+            }
+        }
+        public void sumaDeColumnas()
+        {
+
+            for (int i = 0; i < 7; i++)
+
+            {
+                resultado[i] = 0; //inicializa el array resultado en 0
+                for (int j = 0; j < 7; j++)
+                {
+                    resultado[i] += numeros[j, i]; //suma los valores de cada columna
+                }
+
+                Console.Write(+resultado[i] + "\t\t");
+            }
+
+
+        }
+        public void sumaDeFilas()
+        {
+            for (int i = 0; i < 7; i++)
+
+            {
+                resultado[i] = 0; //inicializa el array resultado en 0
+                for (int j = 0; j < 7; j++)
+                {
+                    resultado[i] += numeros[j, i]; //suma los valores de cada columna
+                }
+
+                Console.WriteLine(+resultado[i] + "\t\t");
+            }
+        }
+
+
+        public void mostrarDatos()
+        {
+            Console.WriteLine("ARTICULOS \tLunes\t\tMartes\t\tMiercoles\tJueves\t\tViernes\t\tSábado\t\tDomingo\t\tTOTAL ");
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                Console.Write("Articulo " + dias[i] + " :     ");
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    Console.Write(numeros[i, j] + "  \t\t");
+                }
+                ;
+            }
+            Console.Write("Ventas diarias   ");
+        }
+
+        int[] menorVenta = new int[7];
+        int[] mayorVenta = new int[7];
+        public void CalcularVentaasManMiv()
+        {
+
+            for (int i = 0; i < 7; i++)
+            {
+                menorVenta[i] = numeros[i, 0];
+                mayorVenta[i] = numeros[i, 0];
+                for (int j = 0; j < 7; j++)
+                {
+                    if (numeros[i, j] < menorVenta[i])
+                    {
+                        menorVenta[i] = numeros[i, j];
+                    }
+                    if (numeros[i, j] > mayorVenta[i])
+                    {
+                        mayorVenta[i] = numeros[i, j];
+                    }
+                }
+            }
+        }
+        public void mostrarVentasMinMax()
+        {
+            CalcularVentaasManMiv();
+
+            Console.WriteLine("\n--- Registro de ventas mínimas y máximas por artículo ---");
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Artículo " + (i + 1) + ": venta mínima registrada: " + menorVenta[i] +
+                                  " | venta máxima registrada: " + mayorVenta[i]);
+            }
+        }
+
+        public void CalcularTotalesSemanales()
+        {
+            int totalSemanal = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                totalSemanal += resultado[i];
+            }
+            Console.WriteLine("\nTotal semanal de ventas: " + totalSemanal);
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+    
+}
