945cac6ce6bd0ffc92bdd64823f5bb2dbb612f78
LeonardoPenas <n00460162@upn.pe>
2025-07-09 22:33:13 -0500

    EXAMENFINAL_ERICK PENAS

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiBiblioteca;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Clase_EP1 clase = new Clase_EP1();
+         //clase.valores();
+         //clase.sumaDeColumnas(7);
+            clase.articulo1();
+            clase.articulo2();
+            clase.articulo3();
+            clase.articulo4();
+            clase.articulo5();
+            clase.articulo6();
+            clase.articulo7();
+            clase.sumaDeFilas();
+            clase.mostrarDatos();
+            clase.sumaDeColumnas();
+            clase.mostrarVentasMinMax();
+            clase.CalcularTotalesSemanales();
+
+            //clase.sumaDeFilas();
+            
+
+            Console.ReadKey();
+
+        }
+
+
+        
+
+    }
+}
+
